bf60bf7888a81aee20a882ccafbabaf36f2e10f6
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-10-30 17:37:12 -0700

    moved images in pogo

--- pogo-fish/Pogo1.py
+++ pogo-fish/Pogo1.py
@@ -5,7 +5,7 @@ WinWidth = 1800
 WinHeight = 900
 win = pygame.display.set_mode((WinWidth, WinHeight))
 clock = pygame.time.Clock()
-bg = pygame.image.load('Level1.png')
+bg = pygame.image.load('images/Level1.png')
 pygame.display.set_caption("Pogo Fish")
 sound1 = pygame.mixer.Sound('Sound1.wav')
 pygame.mixer.music.load('Music.mp3')
@@ -59,7 +59,7 @@ class Player:
 		self.player = pygame.Rect(self.x, self.y, self.width, self.height)
 
 class Objects:
-	def __init__(self, x, y, width, height, imageRight = 'Blank.png', imageLeft = 'Blank.png',  image = 'Blank.png'):
+	def __init__(self, x, y, width, height, imageRight = 'images/Blank.png', imageLeft = 'images/Blank.png',  image = 'images/Blank.png'):
 		self.img = pygame.image.load(image)
 		self.object = pygame.Rect(x, y, width, height)
 		self.moveRight = pygame.image.load(imageRight)
@@ -179,48 +179,48 @@ def redraw():
 	player.draw(win)
 	pygame.display.update()
 
-player = Player('FishR.png', 'FishL.png', 'FishS.png', 'FishD.png', 'FishX.png', 275, pGround, 150, 200)
+player = Player('images/FishR.png', 'images/FishL.png', 'images/FishS.png', 'images/FishD.png', 'images/FishX.png', 275, pGround, 150, 200)
 
-coral1 = Objects(425, ground, 150, 150, image='CoralPink.png')
-coral2 = Objects(775, ground, 150, 150, image='CoralRed.png')
-coral3 = Objects(1725, ground, 150, 150, image='CoralBlue.png')
-coral4 = Objects(2350, ground, 150, 150, image='CoralYellow.png')
-coral5 = Objects(4000, ground, 150, 150, image='CoralYellow.png', )
-coral6 = Objects(5150, ground, 150, 150, image='CoralPink.png', )
-coral7 = Objects(5300, ground, 150, 150, image='CoralBlue.png', )
-coral8 = Objects(5450, ground, 150, 150, image='CoralPurple.png', )
-coral9 = Objects(2500, ground, 150, 150, image='CoralGreen.png', )
+coral1 = Objects(425, ground, 150, 150, image='images/CoralPink.png')
+coral2 = Objects(775, ground, 150, 150, image='images/CoralRed.png')
+coral3 = Objects(1725, ground, 150, 150, image='images/CoralBlue.png')
+coral4 = Objects(2350, ground, 150, 150, image='images/CoralYellow.png')
+coral5 = Objects(4000, ground, 150, 150, image='images/CoralYellow.png', )
+coral6 = Objects(5150, ground, 150, 150, image='images/CoralPink.png', )
+coral7 = Objects(5300, ground, 150, 150, image='images/CoralBlue.png', )
+coral8 = Objects(5450, ground, 150, 150, image='images/CoralPurple.png', )
+coral9 = Objects(2500, ground, 150, 150, image='images/CoralGreen.png', )
 
-coralB1 = Objects(2500, ground-150, 150, 150, image='CoralPurple.png')
-coralB2 = Objects(2650, ground-150, 150, 150, image='CoralPink.png')
-coralB3 = Objects(5300, ground-150, 150, 150, image='CoralGreen.png')
-coralB4 = Objects(5450, ground-150, 150, 150, image='CoralYellow.png')
+coralB1 = Objects(2500, ground-150, 150, 150, image='images/CoralPurple.png')
+coralB2 = Objects(2650, ground-150, 150, 150, image='images/CoralPink.png')
+coralB3 = Objects(5300, ground-150, 150, 150, image='images/CoralGreen.png')
+coralB4 = Objects(5450, ground-150, 150, 150, image='images/CoralYellow.png')
 
-coralC1 = Objects(5450, ground-300, 150, 150, image='CoralRed.png')
+coralC1 = Objects(5450, ground-300, 150, 150, image='images/CoralRed.png')
 
-krab1 = Objects(1000, ground, 225, 150, 'KrabR.png', 'KrabL.png')
-krab2 = Objects(3000, ground, 225, 150, 'KrabR.png', 'KrabL.png')
+krab1 = Objects(1000, ground, 225, 150, 'images/KrabR.png', 'images/KrabL.png')
+krab2 = Objects(3000, ground, 225, 150, 'images/KrabR.png', 'images/KrabL.png')
 
-hole1 = Objects(600, ground+149, 150, 50, image='Hole1.png')
-hole2 = Objects(1500, ground+149, 150, 50, image='Hole1.png')
+hole1 = Objects(600, ground+149, 150, 50, image='images/Hole1.png')
+hole2 = Objects(1500, ground+149, 150, 50, image='images/Hole1.png')
 
-heart1 = Objects(1700,0,50,50,image='Heart1.png')
-heart2 = Objects(1600,0,50,50,image='Heart1.png')
-heart3 = Objects(1500,0,50,50,image='Heart1.png')
+heart1 = Objects(1700,0,50,50,image='images/Heart1.png')
+heart2 = Objects(1600,0,50,50,image='images/Heart1.png')
+heart3 = Objects(1500,0,50,50,image='images/Heart1.png')
 
-sand1 = Objects(0, 750, 150, 150,image='Sand.png')
-sand2 = Objects(150, 750, 150, 150,image='Sand.png')
-sand3 = Objects(300, 750, 150, 150,image='Sand.png')
-sand4 = Objects(450, 750, 150, 150,image='Sand.png')
-sand5 = Objects(600, 750, 150, 150,image='Sand.png')
-sand6 = Objects(750, 750, 150, 150,image='Sand.png')
-sand7 = Objects(900, 750, 150, 150,image='Sand.png')
-sand8 = Objects(1050, 750, 150, 150,image='Sand.png')
-sand9 = Objects(1200, 750, 150, 150,image='Sand.png')
-sand10 = Objects(1350, 750, 150, 150,image='Sand.png')
-sand11 = Objects(1500, 750, 150, 150,image='Sand.png')
-sand12 = Objects(1650, 750, 150, 150,image='Sand.png')
-sand13 = Objects(1800, 750, 150, 150,image='Sand.png')
+sand1 = Objects(0, 750, 150, 150,image='images/Sand.png')
+sand2 = Objects(150, 750, 150, 150,image='images/Sand.png')
+sand3 = Objects(300, 750, 150, 150,image='images/Sand.png')
+sand4 = Objects(450, 750, 150, 150,image='images/Sand.png')
+sand5 = Objects(600, 750, 150, 150,image='images/Sand.png')
+sand6 = Objects(750, 750, 150, 150,image='images/Sand.png')
+sand7 = Objects(900, 750, 150, 150,image='images/Sand.png')
+sand8 = Objects(1050, 750, 150, 150,image='images/Sand.png')
+sand9 = Objects(1200, 750, 150, 150,image='images/Sand.png')
+sand10 = Objects(1350, 750, 150, 150,image='images/Sand.png')
+sand11 = Objects(1500, 750, 150, 150,image='images/Sand.png')
+sand12 = Objects(1650, 750, 150, 150,image='images/Sand.png')
+sand13 = Objects(1800, 750, 150, 150,image='images/Sand.png')
 
 container = Container()
 
@@ -266,7 +266,6 @@ while True:
 						((player.player.left >= coral.object.left and player.player.left <= coral.object.right) or \
 						(player.player.right <= coral.object.right and player.player.right >= coral.object.left))):
 						player.y = coral.object.top
-						print ('over a coral')
 						break
 						pass
 				else:
